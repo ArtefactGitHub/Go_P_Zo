990ba54e08ca1efe35ef46c7d26ce512b6b6c267
ArtefactGitHub <artefactgo@gmail.com>
2021-12-09 16:09:28 +0900

    ymlファイルから設定を取得する処理を追加

--- src/go.mod
+++ src/go.mod
@@ -2,4 +2,7 @@ module github.com/ArtefactGitHub/Go_T_TestDBAccess
 
 go 1.17
 
-require github.com/go-sql-driver/mysql v1.6.0 // indirect
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	confContent, err := ioutil.ReadFile("conf.yml")
+	if err != nil {
+		panic(err)
+	}
+	conf := &Config{}
+	if err := yaml.Unmarshal(confContent, conf); err != nil {
+		panic(err)
+	}
+	fmt.Printf("config: %v\n", conf)
+}
+
+type Config struct {
+	Environment string `yaml:"environment"`
+	Key         string `yaml:"key"`
 }
