fc8ed914dea050fac6b33d70597881c97c6ea3f5
ArtefactGitHub <artefactgo@gmail.com>
2022-01-30 21:32:37 +0900

    アクセストークン取得処理をserviceへ移行

--- internal/api/v1/client/controller.go
+++ internal/api/v1/client/controller.go
@@ -5,13 +5,9 @@ import (
 	"errors"
 	"log"
 	"net/http"
-	"time"
 
-	"github.com/ArtefactGitHub/Go_P_Zo/internal/config"
-	"github.com/ArtefactGitHub/Go_P_Zo/internal/platform/myauth"
 	"github.com/ArtefactGitHub/Go_P_Zo/internal/platform/myhttp"
 	"github.com/ArtefactGitHub/Go_P_Zo/pkg/common"
-	"github.com/golang-jwt/jwt"
 )
 
 type clientController struct {
@@ -37,7 +33,7 @@ func (c *clientController) post(w http.ResponseWriter, r *http.Request, ps commo
 		return
 	}
 
-	token, err := createAccessToken()
+	token, err := c.s.createAccessToken()
 	if err != nil {
 		myhttp.Write(w, NewPostResponse(myhttp.NewResponse(err, http.StatusInternalServerError, ""), nil), http.StatusInternalServerError)
 		return
@@ -56,24 +52,4 @@ func (c *clientController) contentToModel(r *http.Request) (*Client, error) {
 		return nil, err
 	}
 	return &result, nil
-}
-
-func createAccessToken() (*AccessToken, error) {
-	claims := myauth.AuthClaims{StandardClaims: &jwt.StandardClaims{
-		ExpiresAt: time.Now().Add(time.Minute * time.Duration(config.Cfg.Auth.TokenExpiration)).Unix(),
-		Issuer:    "zo.auth.service",
-	},
-		TokenType: "access",
-	}
-
-	// https://pkg.go.dev/github.com/golang-jwt/jwt#NewWithClaims
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	jwt, err := token.SignedString([]byte(config.Cfg.Auth.SignKey))
-	log.Printf("signed: %v", jwt)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &AccessToken{Jwt: jwt, ExpiresAt: claims.ExpiresAt}, nil
 }
