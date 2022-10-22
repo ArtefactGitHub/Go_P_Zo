12b8e6290da9889dc343f4359807154bfbb519fc
ArtefactGitHub <artefactgo@gmail.com>
2022-10-22 17:30:14 +0900

    lintエラー修正 SA1019: "io/ioutil" has been deprecated since Go 1.16: As of Go 1.16, the same functionality is now provided by package io or package os, and those implementations should be preferred in new code. See the specific function documentation for details. (staticcheck)

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"io/ioutil"
 	"os"
 
 	"gopkg.in/yaml.v2"
@@ -32,7 +31,7 @@ var Cfg *Config
 // 秘匿情報は環境変数から読み込みます
 // https://mtyurt.net/post/go-using-environment-variables-in-configuration-files.html
 func LoadConfig(filePath string) (config *Config, err error) {
-	confContent, err := ioutil.ReadFile(filePath)
+	confContent, err := os.ReadFile(filePath)
 	if err != nil {
 		return nil, err
 	}
