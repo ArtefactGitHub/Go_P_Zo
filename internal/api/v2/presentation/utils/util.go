cb8c03acb1cb068d5a794ae70259239c1d91cac6
ArtefactGitHub <artefactgo@gmail.com>
2023-05-07 11:31:39 +0900

    handler層のユーティリティ関数の追加

---
+++ internal/api/v2/presentation/utils/util.go
@@ -0,0 +1,67 @@
+package util
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	derr "github.com/ArtefactGitHub/Go_P_Zo/internal/api/v2/domain/error"
+	"github.com/ArtefactGitHub/Go_P_Zo/internal/platform/myauth"
+	"github.com/ArtefactGitHub/Go_P_Zo/internal/platform/mycontext"
+	"github.com/ArtefactGitHub/Go_P_Zo/internal/platform/myhttp"
+	"github.com/ArtefactGitHub/Go_P_Zo/pkg/common"
+)
+
+const ResourceIdZo = "zo_id"
+
+func GetUserIdFromToken(ctx context.Context) (int, error) {
+	// ユーザートークンの取得
+	userToken, err := mycontext.FromContextStr(ctx, mycontext.UserTokenKey)
+	if err != nil {
+		return -1, err
+	}
+
+	// ユーザートークンからユーザーIDの取得
+	claims, err := myauth.CreateUserTokenClaims(userToken)
+	if err != nil {
+		log.Println(err.Error())
+		return -1, err
+	}
+
+	return claims.UserId, nil
+}
+
+func GetResourceId(params common.QueryMap, resourceKey string) (int, error) {
+	idStr, err := GetResourceIdStr(params, resourceKey)
+	if err != nil {
+		return 0, err
+	}
+
+	result, err := strconv.Atoi(idStr)
+	if err != nil {
+		return 0, fmt.Errorf("リソース指定が正しくありません %s", resourceKey)
+	}
+	return result, nil
+}
+
+func GetResourceIdStr(params common.QueryMap, resourceKey string) (string, error) {
+	result := params.Get(resourceKey)
+	if result == "" {
+		return "", fmt.Errorf("リソース指定が必要です %s", resourceKey)
+	}
+	return result, nil
+}
+
+func HandleError(w http.ResponseWriter, err error) {
+	switch {
+	case errors.Is(err, derr.NotFound):
+		myhttp.WriteError(w, err, http.StatusNotFound, "リソースが存在しません")
+	case errors.Is(err, derr.Unauthorized):
+		myhttp.WriteError(w, err, http.StatusUnauthorized, "指定のリソースへアクセスする権限がありません")
+	default:
+		myhttp.WriteError(w, err, http.StatusInternalServerError, "エラーが発生しました")
+	}
+}
