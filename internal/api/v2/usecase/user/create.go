4bfc0551ac9a29ffcb73b6c8e7c57180a97b7a11
ArtefactGitHub <artefactgo@gmail.com>
2023-06-20 22:23:45 +0900

    ユーザー生成時の存在確認を追加

--- internal/api/v2/usecase/user/create.go
+++ internal/api/v2/usecase/user/create.go
@@ -2,7 +2,10 @@ package user
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
+	derr "github.com/ArtefactGitHub/Go_P_Zo/internal/api/v2/domain/error"
 	domain "github.com/ArtefactGitHub/Go_P_Zo/internal/api/v2/domain/user"
 )
 
@@ -20,10 +23,18 @@ func NewCreate(r domain.Repository) Create {
 }
 
 func (u create) Do(ctx context.Context, user domain.User) (domain.User, error) {
-	_, err := u.r.Create(ctx, user)
+	_, err := u.r.FindByIdentifier(ctx, user.Email, user.Password)
+	switch {
+	case err == nil:
+		return domain.User{}, fmt.Errorf("%s: %w", "既に存在するメールアドレスです", derr.Conflict)
+	case !errors.Is(derr.NotFound, err):
+		return domain.User{}, fmt.Errorf("%s: %w", err.Error(), derr.BadRequest)
+	}
+
+	result, err := u.r.Create(ctx, user)
 	if err != nil {
 		return domain.User{}, err
 	}
 
-	return user, nil
+	return result, nil
 }
